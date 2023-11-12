3e3225c139330496ee6af5e88342b0926d4e9a29
Amirani <amiraniburjanadze@gmail.com>
2023-11-12 19:33:17 +0400

    implement dom functionooal

--- script.js
+++ script.js
@@ -1,19 +1,61 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 let apiQuotes = [];
 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
 function newQuote() {
-  const random = Math.floor(Math.random());
-  const quote = apiQuotes[random * apiQuotes.length];
-  console.log(quote);
+  const random = Math.random();
+  const quote =
+    apiQuotes.length !== 0
+      ? apiQuotes[Math.floor(random * apiQuotes.length)]
+      : localQuotes[Math.floor(random * localQuotes.length)];
+
+  authorText.textContent = quote.author.split(",")[0];
+  quoteText.textContent = quote.text;
+
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
 
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
+    complete();
+  } catch (error) {
+  } finally {
     newQuote();
-  } catch (error) {}
+  }
 }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 getQuotes();
